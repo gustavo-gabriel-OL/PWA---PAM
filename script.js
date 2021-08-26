2d01ae936428bb8b8fef9db022bebf55d5129447
Gusta Gabriel  de O.L <85789804+gustavo-gabriel-OL@users.noreply.github.com>
2021-08-26 16:29:14 +0000

    banana

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
 
       // Registro funcionou :)
 
