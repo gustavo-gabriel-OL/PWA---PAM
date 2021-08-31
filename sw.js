6a3ad52e674f3799944943e0f7593a58804ac197
Gusta Gabriel  de O.L <85789804+gustavo-gabriel-OL@users.noreply.github.com>
2021-08-31 14:02:19 +0000

    tudo

--- sw.js
+++ sw.js
@@ -1,22 +1,20 @@
 var cacheName = 'NomeApp-v';
-var  filesToCache = [
+var filesToCache = [
   './',
   './index.html',
   './manifest.json'
 ];
-
-self.addEventListener ('install', function(e){
+self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache){
       return cache.addAll(filesToCache);
     })
   );
 });
-
-self.addEventListener ('fetch', function(e){
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request) .then (function(response){
-      return response || fetch(e.request);
+    caches.match(e.request).then(function(response){
+    return response || fetch(e.request);
     })
   );
 });
